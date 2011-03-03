7acddb2fcea49a83948ae1a89d33976357e95448
Victor Gryshko <stalin1945@gmail.com>
2011-03-03 20:30:04 +0200

    Added: Check for existing service or collection for corresponding model Added: Console output for model generation

--- Skaya/Tool/Project/Provider/XModel.php
+++ Skaya/Tool/Project/Provider/XModel.php
@@ -10,18 +10,29 @@ class Skaya_Tool_Project_Provider_XModel extends Zend_Tool_Project_Provider_Mode
 
 		$profile = $this->_loadProfile();
 
-		$collection = Skaya_Tool_Project_Provider_XCollection::createResource(
-			$profile, $collectionName, $modelSubname, $module
-		);
-		if ($collection) {
-			$collection->create();
+		/**
+		 * @var Zend_Tool_Framework_Client_Response $response
+		 */
+		$response = $this->_registry->getResponse();
+
+		if (!Skaya_Tool_Project_Provider_XCollection::hasResource($profile, $collectionName, $module)) {
+			$collection = Skaya_Tool_Project_Provider_XCollection::createResource(
+				$profile, $collectionName, $modelSubname, $module
+			);
+			if ($collection) {
+				$collection->create();
+				$response->appendContent('Creating collection "' . $collectionName . '" for the model "' . $modelSubname . '"');
+			}
 		}
 
-		$service = Skaya_Tool_Project_Provider_XService::createResource(
-			$profile, $modelSubname, $module
-		);
-		if ($service) {
-			$service->create();
+		if (!Skaya_Tool_Project_Provider_XService::hasResource($profile, $modelSubname, $module)) {
+			$service = Skaya_Tool_Project_Provider_XService::createResource(
+				$profile, $modelSubname, $module
+			);
+			if ($service) {
+				$service->create();
+				$response->appendContent('Creating service "' . $modelSubname . '"');
+			}
 		}
 
 		$this->_storeProfile();
