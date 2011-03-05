5be322b9b798dd578523aee122e3e7a9eed0a93a
Victor Gryshko <stalin1945@gmail.com>
2011-03-05 12:11:02 +0200

    Added: isEmpty method to check whether current model have no data Added: docblocks to the Model abstract class

--- Skaya/Model/Abstract.php
+++ Skaya/Model/Abstract.php
@@ -1,22 +1,50 @@
 <?php
 
 abstract class Skaya_Model_Abstract implements Skaya_Model_Interface {
-	
+
+	/**
+	 * Database mapper type
+	 */
 	const MAPPER_DATABASE = 'db';
-	const MAPPER_SESSION = 'session';
-	const MAPPER_COUCHDB = 'couchdb'; 
-	
+
+	/**
+	 * Session mapper type
+	 */
+	const MAPPER_SESSION = 'session'; 
+
+	/**
+	 * Model data
+	 * @var array
+	 */
 	protected $_data = array();
-	
+
+	/**
+	 * Name of the Model
+	 * @var string
+	 */
 	protected $_modelName = '';
-    
+
+	/**
+	 * Mapper broker
+	 * @var Skaya_Model_Mapper_MapperBroker
+	 */
     public $mappers;
-	
+
+	/**
+	 * Creates Model object
+	 * @param array $data
+	 */
 	public function __construct($data = array()) {
         $this->mappers = Skaya_Model_Mapper_MapperBroker::getInstance();
 		if (!empty($data)) $this->populate($data);
 	}
-	
+
+	/**
+	 * Populate Model with data
+	 * @throws Skaya_Model_Exception
+	 * @param array $data
+	 * @return Skaya_Model_Abstract
+	 */
 	public function populate($data = array()) {
 		if (is_object($data)) {
 			if (method_exists($data, 'toArray')) {
@@ -37,7 +65,22 @@ abstract class Skaya_Model_Abstract implements Skaya_Model_Interface {
 		
 		return $this;
 	}
-	
+
+	/**
+	 * Returns true if no data stored in Model
+	 * @return bool
+	 */
+	public function isEmpty() {
+		return empty($this->_data);
+	}
+
+	/**
+	 * Set model property
+	 * @throws Skaya_Model_Exception
+	 * @param  $name
+	 * @param  $value
+	 * @return void
+	 */
 	public function __set($name, $value) {
 		if (!is_string($name) || empty($name)) {
 			throw new Skaya_Model_Exception('Name cannot be empty');
@@ -53,7 +96,12 @@ abstract class Skaya_Model_Abstract implements Skaya_Model_Interface {
 			$this->_data[$name] = $value;
 		}
 	}
-	
+
+	/**
+	 * Get model property
+	 * @param  $name
+	 * @return mixed|null
+	 */
 	public function __get($name) {
 		$camelcaseFilter = new Zend_Filter_Word_UnderscoreToCamelCase();
 		$getterName = 'get' . ucfirst($camelcaseFilter->filter($name));
@@ -67,17 +115,31 @@ abstract class Skaya_Model_Abstract implements Skaya_Model_Interface {
 		}
 		return $data;
 	} 
-	
+
+	/**
+	 * Check whether there is some data in the model
+	 * @param  $name
+	 * @return bool
+	 */
 	public function __isset($name) {
 		return array_key_exists($name, $this->_data);
 	}
-	
+
+	/**
+	 * Unset model property with given name
+	 * @param  $name
+	 * @return void
+	 */
 	public function __unset($name) {
 		if (array_key_exists($name, $this->_data)) {
 			unset($this->_data[$name]);
 		}
 	}
-	
+
+	/**
+	 * Converts model to array
+	 * @return array
+	 */
 	public function toArray() {
 		$data = (array)$this->_data;
 		foreach ($data as $key => &$value) {
@@ -92,17 +154,29 @@ abstract class Skaya_Model_Abstract implements Skaya_Model_Interface {
 		}
 		return $data;
 	}
-	
+
+	/**
+	 * Saves model data using current model's mapper
+	 * @return Skaya_Model_Abstract
+	 */
 	public function save() {
 		$data = $this->getMapper()->save($this->toArray());
 		return $this->populate($data);
 	}
-	
+
+	/**
+	 * Delete model from the storage
+	 * @return Skaya_Model_Abstract
+	 */
 	public function delete() {
 		$this->getMapper()->delete($this->toArray());
 		return $this;
 	}
 
+	/**
+	 * Returns current model's mapper
+	 * @return Skaya_Model_Mapper_Interface
+	 */
     public function getMapper() {
         return $this->mappers->getMapper($this->_modelName);
     }
